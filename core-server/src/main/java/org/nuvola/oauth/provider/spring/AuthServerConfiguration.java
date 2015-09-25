ba057710822ec46b244aa5a4cab679e668f4778a
imrabti <imrabti@gmail.com>
2015-09-25 17:59:53 +0100

    Refactored the configuration.

---
+++ core-server/src/main/java/org/nuvola/oauth/provider/spring/AuthServerConfiguration.java
@@ -0,0 +1,115 @@
+package org.nuvola.oauth.provider.spring;
+
+import java.security.Principal;
+
+import javax.sql.DataSource;
+
+import org.nuvola.oauth.provider.security.CustomAuthenticationProvider;
+import org.nuvola.oauth.provider.security.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@RestController
+@SessionAttributes("authorizationRequest")
+@EnableResourceServer
+@Configuration
+public class AuthServerConfiguration extends WebMvcConfigurerAdapter {
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+    }
+
+    @Configuration
+    @Order(-11)
+    protected static class LoginConfig extends WebSecurityConfigurerAdapter {
+        @Autowired
+        private MyUserDetailsService userDetailsService;
+
+        @Bean
+        public AuthenticationProvider authenticationProvider() {
+            DaoAuthenticationProvider authenticationProvider = new CustomAuthenticationProvider();
+            authenticationProvider.setUserDetailsService(userDetailsService);
+            authenticationProvider.setPasswordEncoder(new ShaPasswordEncoder());
+
+            return authenticationProvider;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .formLogin().loginPage("/login").permitAll()
+                    .and()
+                    .requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+                    .and()
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .authenticationProvider(authenticationProvider());
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(authenticationProvider());
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+        @Autowired
+        private AuthenticationManager authenticationManager;
+        @Autowired
+        private DataSource dataSource;
+
+        public JdbcTokenStore jdbcTokenStore() {
+            return new JdbcTokenStore(dataSource);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.jdbc(dataSource)
+                    .withClient("myapp")
+                    .secret("KDV3FT2wCMnmzwzH")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "password")
+                    .scopes("openid")
+                    .autoApprove(true);
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager).tokenStore(jdbcTokenStore());
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+        }
+    }
+}
