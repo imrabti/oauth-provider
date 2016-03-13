e126946224e7d27b2afc4c66dc1ed4c6a7aafe70
imrabti <imrabti@gmail.com>
2016-03-13 22:10:53 +0000

    Simplify the SecurityContext user Jackson ObjectMapper to parse the UserProfile object.

--- sso-client/sso-server/src/main/java/org/nuvola/oauth/server/security/impl/SecurityContextImpl.java
+++ sso-client/sso-server/src/main/java/org/nuvola/oauth/server/security/impl/SecurityContextImpl.java
@@ -1,42 +1,34 @@
 package org.nuvola.oauth.server.security.impl;
 
 import java.security.Principal;
-import java.util.List;
 import java.util.Map;
 
 import org.nuvola.oauth.server.security.SecurityContext;
-import org.nuvola.oauth.shared.ApplicationAuthority;
 import org.nuvola.oauth.shared.UserProfile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Component
 public class SecurityContextImpl implements SecurityContext {
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    SecurityContextImpl(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public UserProfile getCurrentUser(Principal user) {
         OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) user;
         Authentication authentication = oAuth2Authentication.getUserAuthentication();
 
         if (oAuth2Authentication.isAuthenticated() && authentication != null && authentication.getDetails() != null) {
-            Map<String, Object> principal = (Map) ((Map) authentication.getDetails()).get("principal");
-            Map<String, Object> info = (Map) principal.get("userProfile");
-
-            UserProfile profile = new UserProfile();
-            profile.setUserName((String) info.get("userName"));
-            profile.setFirstName((String) info.get("firstName"));
-            profile.setLastName((String) info.get("lastName"));
-            profile.setEmail((String) info.get("email"));
-
-            if (info.containsKey("authorities")) {
-                List<Map<String, Object>> authorities = (List) info.get("authorities");
-                for (Map<String, Object> authority : authorities) {
-                    ApplicationAuthority applicationAuthority = new ApplicationAuthority();
-                    applicationAuthority.setClientId((String) authority.get("clientId"));
-                    applicationAuthority.setAuthority((String) authority.get("authority"));
-                    profile.getAuthorities().add(applicationAuthority);
-                }
-            }
+            Map principal = (Map) ((Map) authentication.getDetails()).get("principal");
+            UserProfile profile = objectMapper.convertValue(principal.get("userProfile"), UserProfile.class);
 
             return profile;
         }
